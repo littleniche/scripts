4a96a5bcf47367ea0e67b4c59028683202caad1c
akshaykhoje <khojeakshay02@gmail.com>
2022-08-20 11:47:07 +0530

    uploaded ubuntu initial setup script

---
+++ ubuntu2204.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# go to software and updates and tick all the four "Downloadable from the Internet"
+
+# sudo apt-get update && sudo apt-get upgrade -y
+
+# If you're unable to play media codecs, uncomment the following lines
+# sudo apt install ubuntu-restricted-extras
+
+# Check your "Software Updates" settings.
+
+# For extra customization and extensions
+# sudo add-apt-repository universe
+# sudo apt install gnome-tweak-tool
+
+# Install firewall
+# sudo apt-get install gufw
+# sudo ufw enable
+
+# Install a better package manager - Synaptic
+# sudo apt-get install synaptic
+
+# sudo apt install git
+
+# sudo apt install wireshark
+
+# Install snapd and snap
+# sudo apt install snapd
+# sudo snap install core
+# check if snapd is running successfully without any error
+# sudo systemctl status snapd
+# In case it does not start, uncomment the following command to start it.
+# sudo systemctl start snapd
+# sudo snap install snap-store
+
+# sudo snap install code --classic
+# sudo snap install vlc
+
+# Install flatpak
+# sudo apt-get install flatpak
+# sudo apt-get install gnome-software-plugin-flatpak
+# flatpak remote-add --if-not-exists flathub https://flathub-org/repo/flathub.flatpakrepo
+
+# sudo apt install wireshark
+
