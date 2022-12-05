11700ae29beb69d1fd8e737d3162a3ebea334b5d
akshaykhoje <khojeakshay02@gmail.com>
2022-12-05 15:57:43 +0530

    Added Proxy folder

---
+++ Proxy/proxy2.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+global_proxy="/etc/profile.d/proxy.sh"
+wget_proxy="/etc/wgetrc"
+apt_proxy="/etc/apt/apt.conf.d/80proxy"
+
+
+greeting()
+{
+	echo "------------------------------------------------------------------------------"
+	echo "			  PROXY INSTALLATION PROGRAM!!!			     "
+	echo -e "------------------------------------------------------------------------------\n"
+	
+}
+
+
+createFiles()
+{
+	if [ ! -f /etc/profile.d/proxy.sh ];
+	then
+		touch /etc/profile.d/proxy.sh
+	fi
+	
+	if [ ! -f /etc/apt/apt.conf.d/80proxy ]; 
+	then
+		touch /etc/apt/apt.conf.d/80proxy
+	fi
+
+	if [ ! -f ~/.wgetrc ];
+	then
+		touch /etc/wgetrc
+	fi
+}
+
+
+userInfo()
+{
+	echo "Initializing proxy setup process..."
+	read -p "Enter IP address of PROXY SERVER : " proxy_server
+	read -p "Enter the port number : " port
+	read -p "Enter the username : " username
+	read -p "Enter the password : " password
+
+	echo "$proxyServer $port $username $password"
+}
+
+
+globeAndCurl()
+{
+	cat <<EOF > $global_proxy
+export http_proxy="http://$username:$password@$proxy_server:$port/"
+export https_proxy="http://$username:$password@$proxy_server:$port/"
+export ftp_proxy="http://$username:$password@$proxy_server:$port/"
+export no_proxy="127.0.0.1,localhost"
+
+
+export HTTP_PROXY="http://$username:$password@$proxy_server:$port/"
+export HTTPS_PROXY="http://$username:$password@$prox_server:$port/"
+export FTP_PROXY="http://$username:$password@$proxy_server:$port/"
+export NO_PROXY="127.0.0.1,localhost"	
+EOF
+}
+
+
+wgetRC()
+{
+	cat <<EOF > $wget_proxy
+use_proxy = on
+http_proxy = http://$username:$password@$proxy_server:$port/
+https_proxy = http://$username:$password@$proxy_server:$port/
+ftp_proxy = http://$username:$password@$proxy_server:$port/
+EOF
+}
+
+
+aptProxy()
+{
+	cat <<EOF > $apt_proxy
+Acquire::http::proxy "http://$username:$password@$proxy_server:$port/";
+Acquire::https::proxy "http://$username:$password@$proxy_server:$port/";
+Acquire::ftp::proxy "http://$username:$password@$proxy_server:$port/";
+EOF
+}
+
+
+
+
+
+
+greeting
+createFiles
+userInfo
+globeAndCurl
+wgetRC
+aptProxy
+
---
+++ Proxy/uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# to uninstall apt proxy configuration
+sudo rm /etc/apt/apt.conf.d/80proxy
+
+# to uninstall wget proxy configurations
+sudo rm /etc/wgetrc
+
+# to uninstall system-wide proxy
+sudo rm /etc/profile.d/proxy.sh
+
+
